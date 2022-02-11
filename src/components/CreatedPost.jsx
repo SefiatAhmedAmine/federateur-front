c5b568df44e62d199a769ee221639ef05e2b4d26
OussamaAarab <oussamaaarab5712@gmail.com>
2022-02-12 00:51:44 +0100

    Title

--- src/components/CreatedPost.jsx
+++ src/components/CreatedPost.jsx
@@ -10,24 +10,14 @@ function CreatedPost() {
     const [categories, setCategories] = useState([])
 
     const [formulaire, setFormulaire] = useState({
-        "id": 26,
+        "title": "",
         "category": null,
         "description": "",
         "createdAt": "2016-02-29T22:02:03.752Z",
         "responds": [],
         "available": true,
         "type": "",
-        "createdBy": {
-            "id": 1,
-            "firstname": "Oussama",
-            "lastname": "Aarab",
-            "phoneNumber": "0624085197",
-            "longitude": 6.3755290673898965E7,
-            "latitude": -4.356204952808507E7,
-            "email": "oussama_aarab@um5.ac.ma",
-            "password": "$2a$10$4f3fZWXHkRLybyg61e4D..7xMH9xg6jX21P0t3I/UiGm/MSejjpMC",
-            "roles": ["USER"]
-        }
+        "createdBy": JSON.parse(localStorage.getItem("user"))
     })
 
     useEffect(
@@ -35,9 +25,8 @@ function CreatedPost() {
             async function getDataCategories() {
                 try {
                     const response = await getCategory();
-                    //console.log(response);
                     setCategories(response);
-                    
+
                 } catch ({ response }) {
                     console.log(response);
                 }
@@ -47,14 +36,22 @@ function CreatedPost() {
         [setCategories]
     );
 
-    const handleChange = ({currentTarget}) => {
-        const {name, value} = currentTarget;
-        
-        setFormulaire({...formulaire, [name] : value})
+    const handleChange = ({ currentTarget }) => {
+        const { name, value } = currentTarget;
 
-        let data = formulaire;
-        data.category = categories[parseInt(formulaire.category)];
-        setFormulaire(data);
+        setFormulaire({ ...formulaire, [name]: value })
+
+        //let data = formulaire;
+        //data.category = categories[parseInt(formulaire.category)];
+        //setFormulaire(data);
+        console.log(formulaire)
+
+    }
+
+    const handleChangeCategory = (event) => {
+
+        setFormulaire({ ...formulaire, ["category"]: categories[event.target.value] })
+        console.log(event.target.value)
         console.log(formulaire)
 
     }
@@ -63,18 +60,18 @@ function CreatedPost() {
         event.preventDefault();
 
         try {
-            let data = formulaire;
-            data.category = categories[parseInt(formulaire.category)];
-            setFormulaire(data);
+            //let data = formulaire;
+            //data.category = categories[parseInt(formulaire.category)];
+            //setFormulaire(data);
             const submit = await createPost(formulaire);
             console.log(submit);
-        }catch ({ response }) {
-            
-            console.log(formulaire);   
+        } catch ({ response }) {
+
+            console.log("Erreur");
         }
-        
+
     }
-    
+
     return (
         <>
             <section id="appointment" className="appointment section-bg">
@@ -86,20 +83,24 @@ function CreatedPost() {
 
                     <form onSubmit={handleSubmit} className="php-email-form">
 
+                        <div className="form-group mt-3">
+                            <input type="text" className="form-control" name="title" id="title" onChange={handleChange} placeholder="Titre" required />
+                        </div>
+
                         <div className="row">
 
                             <div className="col-md-6 form-group mt-3">
-                                <select name="category" id="category" defaultValue="0" onChange={handleChange} className="form-select">
+                                <select name="category" id="category" defaultValue="0" onChange={handleChangeCategory} className="form-select" required>
                                     <option hidden>Choisir la catégorie</option>
                                     {
-                                        categories.map((cate, index) => (<option key = {index} value={index}>{cate.name}</option>))
-                                    }    
+                                        categories.map((cate, index) => (<option key={cate.name} value={index}>{cate.name}</option>))
+                                    }
                                 </select>
                                 <div className="validate"></div>
                             </div>
 
                             <div className="col-md-6 form-group mt-3">
-                                <select name="type" id="type" defaultValue="0" onChange={handleChange} className="form-select">
+                                <select name="type" id="type" defaultValue="0" onChange={handleChange} className="form-select" required>
                                     <option hidden>Choisir le type</option>
                                     <option value="REQUEST">REQUEST</option>
                                     <option value="Donation">DONATION</option>
@@ -110,7 +111,7 @@ function CreatedPost() {
                         </div>
 
                         <div className="form-group mt-3">
-                            <textarea className="form-control" name="description" onChange={handleChange} rows="5" placeholder="Description"></textarea>
+                            <textarea className="form-control" name="description" onChange={handleChange} rows="5" placeholder="Description" required></textarea>
                             <div className="validate"></div>
                         </div>
 
--- src/components/PostComponent.jsx
+++ src/components/PostComponent.jsx
@@ -20,63 +20,63 @@ export default class PostComponent extends Component {
             <>
                 <body>
                     {/*  <!-- Page content--> */}
-                    <div class="container2 mt-5">
-                        <div class="row">
-                            <div class="col-lg-8">
+                    <div className="container2 mt-5">
+                        <div className="row">
+                            <div className="col-lg-8">
                                 {/*  <!-- Post content--> */}
                                 <article>
                                     {/*  <!-- Post header--> */}
-                                    <header class="mb-4">
+                                    <header className="mb-4">
                                         {/*  <!-- Post title--> */}
-                                        <h1 class="fw-bolder mb-1">name of post </h1>
+                                        <h1 className="fw-bolder mb-1">name of post </h1>
                                         {/*  <!-- Post meta content--> */}
-                                        <div class="text-muted fst-italic mb-2">Date </div>
+                                        <div className="text-muted fst-italic mb-2">Date </div>
                                         {/*  <!-- Post categories--> */}
-                                        <a class="badge bg-secondary text-decoration-none link-light" href="#!"> categorie </a>
+                                        <a className="badge bg-secondary text-decoration-none link-light" href="#!"> categorie </a>
                                     </header>
                                     {/*  <!-- Preview image figure--> */}
-                                    <figure class="mb-4"><img class="img-fluid rounded" src="https://dummyimage.com/900x400/ced4da/6c757d.jpg" alt="..." /></figure>
+                                    <figure className="mb-4"><img className="img-fluid rounded" src="https://dummyimage.com/900x400/ced4da/6c757d.jpg" alt="..." /></figure>
                                     {/*  <!-- Post content--> */}
-                                    <section class="mb-5">
-                                        <p class="fs-5 mb-4">description</p>
+                                    <section className="mb-5">
+                                        <p className="fs-5 mb-4">description</p>
                                     </section>
                                 </article>
                                 {/*  <!-- Comments section--> */}
-                                <section class="mb-5">
-                                    <div class="card bg-light">
-                                        <div class="card-body">
+                                <section className="mb-5">
+                                    <div className="card bg-light">
+                                        <div className="card-body">
                                             {/*  <!-- Comment form--> */}
-                                            <form class="mb-4"><textarea class="form-control" rows="3" placeholder="Join the discussion and leave a comment!"></textarea></form>
+                                            <form className="mb-4"><textarea className="form-control" rows="3" placeholder="Join the discussion and leave a comment!"></textarea></form>
                                             {/*  <!-- Comment with nested comments--> */}
-                                            <div class="d-flex mb-4">
+                                            <div className="d-flex mb-4">
                                                 {/*  <!-- Parent comment--> */}
-                                                <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
-                                                <div class="ms-3">
-                                                    <div class="fw-bold">Commenter Name</div>
+                                                <div className="flex-shrink-0"><img className="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+                                                <div className="ms-3">
+                                                    <div className="fw-bold">Commenter Name</div>
                                                     If you're going to lead a space frontier, it has to be government; it'll never be private enterprise. Because the space frontier is dangerous, and it's expensive, and it has unquantified risks.
                                                     {/*  <!-- Child comment 1--> */}
-                                                    <div class="d-flex mt-4">
-                                                        <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
-                                                        <div class="ms-3">
-                                                            <div class="fw-bold">Commenter Name</div>
+                                                    <div className="d-flex mt-4">
+                                                        <div className="flex-shrink-0"><img className="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+                                                        <div className="ms-3">
+                                                            <div className="fw-bold">Commenter Name</div>
                                                             And under those conditions, you cannot establish a capital-market evaluation of that enterprise. You can't get investors.
                                                         </div>
                                                     </div>
                                                     {/*  <!-- Child comment 2--> */}
-                                                    <div class="d-flex mt-4">
-                                                        <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
-                                                        <div class="ms-3">
-                                                            <div class="fw-bold">Commenter Name</div>
+                                                    <div className="d-flex mt-4">
+                                                        <div className="flex-shrink-0"><img className="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+                                                        <div className="ms-3">
+                                                            <div className="fw-bold">Commenter Name</div>
                                                             When you put money directly to a problem, it makes a good headline.
                                                         </div>
                                                     </div>
                                                 </div>
                                             </div>
                                             {/*  <!-- Single comment--> */}
-                                            <div class="d-flex">
-                                                <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
-                                                <div class="ms-3">
-                                                    <div class="fw-bold">Commenter Name</div>
+                                            <div className="d-flex">
+                                                <div className="flex-shrink-0"><img className="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+                                                <div className="ms-3">
+                                                    <div className="fw-bold">Commenter Name</div>
                                                     When I look at the universe and all the ways the universe wants to kill us, I find it hard to reconcile that with statements of beneficence.
                                                 </div>
                                             </div>
@@ -85,31 +85,31 @@ export default class PostComponent extends Component {
                                 </section>
                             </div>
                             {/*  <!-- Side widgets--> */}
-                            <div class="col-lg-4">
+                            <div className="col-lg-4">
                                 {/*  <!-- Search widget--> */}
-                                <div class="card mb-4">
-                                    <div class="card-header">Search</div>
-                                    <div class="card-body">
-                                        <div class="input-group">
-                                            <input class="form-control" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
-                                            <button class="btn btn-primary" id="button-search" type="button">Go!</button>
+                                <div className="card mb-4">
+                                    <div className="card-header">Search</div>
+                                    <div className="card-body">
+                                        <div className="input-group">
+                                            <input className="form-control" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
+                                            <button className="btn btn-primary" id="button-search" type="button">Go!</button>
                                         </div>
                                     </div>
                                 </div>
                                 {/*  <!-- Categories widget--> */}
-                                <div class="card mb-4">
-                                    <div class="card-header">Categories</div>
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-sm-6">
-                                                <ul class="list-unstyled mb-0">
+                                <div className="card mb-4">
+                                    <div className="card-header">Categories</div>
+                                    <div className="card-body">
+                                        <div className="row">
+                                            <div className="col-sm-6">
+                                                <ul className="list-unstyled mb-0">
                                                     <li><a href="#!">Web Design</a></li>
                                                     <li><a href="#!">HTML</a></li>
                                                     <li><a href="#!">Freebies</a></li>
                                                 </ul>
                                             </div>
-                                            <div class="col-sm-6">
-                                                <ul class="list-unstyled mb-0">
+                                            <div className="col-sm-6">
+                                                <ul className="list-unstyled mb-0">
                                                     <li><a href="#!">JavaScript</a></li>
                                                     <li><a href="#!">CSS</a></li>
                                                     <li><a href="#!">Tutorials</a></li>
