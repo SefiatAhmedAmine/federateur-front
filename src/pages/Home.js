af95738af93cc02c5def22c1a4a841b298948ceb
SAID El ABOUDI <sdelaboudi@gmail.com>
2022-02-12 02:24:13 +0100

    production *-

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,31 +3,6 @@ import sadaka from '../images/sadaka.png'
 import legs from '../images/legs.png'
 import orphelin from '../images/orphelin.png'
 const Home = () => {
-    window.localStorage.setItem('user', JSON.stringify(
-        {
-            "id": 1,
-            "firstname": "Oussama",
-            "lastname": "Aarab",
-            "phoneNumber": "0624085197",
-            "longitude": 6.3755290673898965E7,
-            "latitude": -4.356204952808507E7,
-            "email": "oussama_aarab@um5.ac.ma",
-            "password": "$2a$10$4f3fZWXHkRLybyg61e4D..7xMH9xg6jX21P0t3I/UiGm/MSejjpMC",
-            "roles": [
-                "USER"
-            ],
-            "accountNonExpired": true,
-            "accountNonLocked": true,
-            "credentialsNonExpired": true,
-            "authorities": [
-                {
-                    "authority": "USER"
-                }
-            ],
-            "username": "oussama_aarab@um5.ac.ma",
-            "enabled": true
-        }
-    ));
     return (
         <>
             <br /><br />
