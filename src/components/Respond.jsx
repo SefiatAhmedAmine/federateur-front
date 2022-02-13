6e66aba2b5bd0d702b6ca463efc898e51b3d2536
SAID El ABOUDI <sdelaboudi@gmail.com>
2022-02-13 22:44:20 +0100

    production v1.2

--- src/components/Respond.jsx
+++ src/components/Respond.jsx
@@ -15,21 +15,35 @@ const Respond = () => {
                                 </div>
                                 <div class="d-flex flex-row mt-1 ellipsis"> <small class="mr-2">le :{response.createdAt}</small> <i class="fa fa-ellipsis-h"></i> </div>
                             </div>
-                            <img src="https://i.imgur.com/xhzhaGA.jpg" class="img-fluid" alt="img" />
+                            <img src={response.imageLink} class="img-fluid" alt="img" />
                             <div class="p-2">
                                 description :
                                 <p class="text-justify"> {response.description}</p>
                                 <hr />
                                 <div class="d-flex justify-content-between align-items-center">
                                     <div class="d-flex flex-row icons d-flex align-items-center"> <i class="fa fa-heart"></i> <i class="fa fa-smile-o ml-2"></i> </div>
-                                    <div class="d-flex flex-row muted-color"> <span>Available : {response.available}</span> <span class="ml-2"></span> </div>
+                                    <div class="d-flex flex-row muted-color">
+                                        {(response.available) && (<>
+                                            <span>Available : Oui</span>
+                                        </>)
+                                        }
+                                        <span class="ml-2"></span> </div>
                                 </div>
                                 <hr />
                                 <div class="comments">
                                     votre réponse :
                                     <div class="d-flex flex-row mb-2">
                                         <div class="d-flex flex-column ml-2"> <small class="comment-text">{response.responds[0].message}</small>
-                                            <div class="d-flex flex-row align-items-center status"> <small>Verifié : {response.responds[0].verified}</small><small></small> </div>
+                                            <div class="d-flex flex-row align-items-center status">
+                                                {(response.responds[0].verified) && (<>
+                                                    <small>Verifié : Oui</small>
+                                                </>) || (<>
+                                                    <small>Verifié : Non</small>
+                                                </>)
+                                                }
+
+
+                                                <small></small> </div>
                                         </div>
                                     </div>
                                 </div>
